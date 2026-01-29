e2ff1c1629eb3fde19283486f513228c2c103fe2
Frank Froggins <pearlover912@gmail.com>
2026-01-29 10:06:42 -0500

    Chore: Fixed cpack deb generator using amd64 regardless of architecture

--- cmake/CPackConfig.cmake
+++ cmake/CPackConfig.cmake
@@ -27,7 +27,6 @@ set(CPACK_RESOURCE_FILE_README "${CMAKE_SOURCE_DIR}/README.md")
 if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
     set(CPACK_BINARY_DEB ON)
     set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
-    set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE amd64)
     set(CPACK_DEBIAN_PACKAGE_SECTION utils)
     # add set(CPACK_DEBIAN_PACKAGE_CONFLICTS <Insert Conflicting Packages Here>) when the other flavors of tb64 are packaged
 endif()
